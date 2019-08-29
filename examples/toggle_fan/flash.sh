a46b5a8c75302b01e36c22050fe776842f9ad895
geory <garbuzov.georgy@gmail.com>
2019-08-29 17:31:13 +0500

    save

---
+++ examples/toggle_fan/flash.sh
@@ -0,0 +1,14 @@
+SONOFF_PORT="/dev/cu.wchusbserial14310"
+
+esptool.py -p $SONOFF_PORT erase_flash
+
+esptool.py \
+            -p $SONOFF_PORT \
+            --baud 115200 \
+            write_flash \
+            -fs 32m \
+            -fm dio \
+            -ff 40m \
+            0x0 ./firmware/rboot.bin \
+            0x1000 ./firmware/blank_config.bin \
+            0x2000 ./firmware/toggle_fan.bin
