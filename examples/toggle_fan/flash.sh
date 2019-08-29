191250c417f0ee2ebff33bfdf81475b0694c139b
geory <garbuzov.georgy@gmail.com>
2019-08-29 17:34:36 +0500

    save

--- examples/toggle_fan/flash.sh
+++ examples/toggle_fan/flash.sh
@@ -9,6 +9,6 @@ esptool.py \
             -fs 32m \
             -fm dio \
             -ff 40m \
-            0x0 ./firmware/rboot.bin \
-            0x1000 ./firmware/blank_config.bin \
+            0x0 ../../rboot.bin \
+            0x1000 ../../blank_config.bin \
             0x2000 ./firmware/toggle_fan.bin
