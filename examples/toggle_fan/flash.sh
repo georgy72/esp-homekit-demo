935589f105cedc40d68b5d7c89e0701c8ff910e2
geory <garbuzov.georgy@gmail.com>
2019-08-29 18:52:28 +0500

    save

--- examples/toggle_fan/flash.sh
+++ examples/toggle_fan/flash.sh
@@ -4,11 +4,11 @@ esptool.py -p $SONOFF_PORT erase_flash
 
 esptool.py \
             -p $SONOFF_PORT \
-            --baud 115200 \
+            --baud 230400 \
             write_flash \
             -fs 32m \
             -fm dio \
             -ff 40m \
-            0x0 ../../rboot.bin \
-            0x1000 ../../blank_config.bin \
+            0x0 ../../firmware/rboot.bin \
+            0x1000 ../../firmware/blank_config.bin \
             0x2000 ./firmware/toggle_fan.bin
