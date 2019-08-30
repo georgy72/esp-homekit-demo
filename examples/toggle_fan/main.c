604195019f30354cd45a48d545cf35827e06d668
geory <garbuzov.georgy@gmail.com>
2019-08-30 21:10:04 +0500

    s

--- examples/toggle_fan/main.c
+++ examples/toggle_fan/main.c
@@ -16,7 +16,7 @@
 
 #define NO_CONNECTION_WATCHDOG_TIMEOUT 120000
 
-const int led_state_gpio_read = 14; //D5 на светодиод
+const int led_state_gpio_read = 5; //D5 на светодиод
 
 const int button_write_one_hours_gpio = 13; //D7 на кнопку один час
 const int button_write_big_gpio = 15; //D8 на большую кнопку
