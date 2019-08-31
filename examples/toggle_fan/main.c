e1efbf45d4a18859b334797e192e27ed1a23b362
georgy <garbuzov.georgy@gmail.com>
2019-08-31 18:14:00 +0500

    s

--- examples/toggle_fan/main.c
+++ examples/toggle_fan/main.c
@@ -107,7 +107,7 @@ void toggle_fan_task(void *_args) {
 }
 
 void toggle_fan() {
-    if (switch_on.value.bool_value != contact_sensor_state_get(led_state_gpio_read)){
+    if (switch_on.value.bool_value == contact_sensor_state_get(led_state_gpio_read)){
         printf("switch_on.value.bool_value: %d ; get contact sensor: %d\n", switch_on.value.bool_value, contact_sensor_state_get(led_state_gpio_read));
         xTaskCreate(toggle_fan_task, "Toggle fan", 128, NULL, 2, NULL);
     }
